a1ee9cca768f49f8eda214698b58a3d0d000239f
mustakimweb <Aghadimustakim786@gmail.com>
2023-08-04 05:43:12 +0500

    date filter work done

--- src/components/pages/Home/DateFilters/index.js
+++ src/components/pages/Home/DateFilters/index.js
@@ -9,6 +9,29 @@ const { RangePicker } = DatePicker;
 
 const DateFilters = ({ handleRange, range }) => {
   console.log("🚀 ~ file: index.js:10 ~ DateFilters ~ range:", range);
+
+  const handelClass = (dateString) => {
+    if (dateString === "Today") {
+      const today = moment();
+      const differenceInDays = range[0]?.diff(today, "days");
+      return differenceInDays === 0 && "Today";
+    } else if (dateString === "Yesterday") {
+      const today = moment();
+      const yesterday = today.subtract(1, "days");
+      return range[0]?.isSame(yesterday, "day") && "Yesterday";
+    } else if (dateString === "Last month") {
+      const today = moment();
+      const lastMonthDate = today.subtract(1, "months");
+      const tomorrow = lastMonthDate.add(1, "days");
+
+      return (
+        range[0]?.isSame(tomorrow, "day") &&
+        range[1]?.isSame(moment(), "day") &&
+        "Last month"
+      );
+    }
+  };
+
   return (
     <Space
       size={20}
@@ -19,7 +42,9 @@ const DateFilters = ({ handleRange, range }) => {
         {DATE_OPTIONS.map((t) => {
           return (
             <div
-              className={`filter-button ${t === range && "active"}`}
+              className={`filter-button ${
+                range ? handelClass(t) === t && "active" : ""
+              }`}
               key={Math.random()}
               onClick={() => handleRange("date", t)}
             >
